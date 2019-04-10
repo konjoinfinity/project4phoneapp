3400c1adaae8140e26a950ec4dff18f54833e9ba
Wesley Scholl <clients@konjoweb.com>
2019-04-10 15:35:44 -0400

    basic scaffolding

---
+++ KonjoCommunities/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Card } from "react-native-elements";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Card>
+        <View>
+          <Text style={{ fontSize: 50, justifyContent: "center", padding: 20 }}>
+            Home
+          </Text>
+        </View>
+      </Card>
+    );
+  }
+}
+
+export default Home;
