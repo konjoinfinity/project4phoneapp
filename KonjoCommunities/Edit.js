28a34c58e3da10a0060c92b4ad06cb72d6ce9d0f
Wesley Scholl <clients@konjoweb.com>
2019-06-10 09:39:26 -0400

    updated text for inputs on edit

--- KonjoCommunities/Edit.js
+++ KonjoCommunities/Edit.js
@@ -275,7 +275,8 @@ const styles = StyleSheet.create({
     fontSize: 25,
     paddingLeft: 20,
     paddingRight: 20,
-    borderRadius: 15
+    borderRadius: 15,
+    textAlign: "center"
   },
   saveButton: {
     borderWidth: 1,
