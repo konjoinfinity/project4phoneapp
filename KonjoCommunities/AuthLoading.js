f7f7f628cc222b95d04c0dfd231a5be955ebc0d1
Wesley Scholl <clients@konjoweb.com>
2019-06-18 14:19:05 -0400

    removed commented lines and underscores in authloading

--- KonjoCommunities/AuthLoading.js
+++ KonjoCommunities/AuthLoading.js
@@ -9,15 +9,11 @@ import AsyncStorage from "@react-native-community/async-storage";
 class AuthLoadingScreen extends React.Component {
     constructor(props) {
         super(props);
-        this._bootstrapAsync();
+        this.bootstrapAsync();
     }
 
-    // Fetch the token from storage then navigate to our appropriate place
-    _bootstrapAsync = async () => {
+    bootstrapAsync = async () => {
         const userToken = await AsyncStorage.getItem('id_token');
-
-        // This will switch to the App screen or Auth screen and this loading
-        // screen will be unmounted and thrown away.
         this.props.navigation.navigate(userToken ? 'App' : 'Auth');
     };
 
