60bec4d81163f4e575802b62bdcd4c6fa8b3a214
Wesley Scholl <clients@konjoweb.com>
2021-02-06 17:53:16 -0700

    stable build, updated text on profile screen

--- KonjoCommunities/Profile.js
+++ KonjoCommunities/Profile.js
@@ -217,7 +217,7 @@ class ProfileScreen extends React.Component {
                     animation="bounceInUp"
                     delay={10}
                     duration={1800}>
-                    <Card title="Profile" titleStyle={{ fontSize: 30, textAlign: "center", padding: 10, fontFamily: 'system font', color: "#000000" }} borderRadius={15}>
+                    <Card title="Profile" titleStyle={{ fontSize: 30, textAlign: "center", padding: 10 }} borderRadius={15}>
                         <Text style={{ fontSize: 30, textAlign: "center", padding: 15 }}>👤 {this.state.creator && this.state.creator}</Text>
                         <Card borderRadius={15} containerStyle={{ backgroundColor: "#12C16D" }}>
                             <View>
