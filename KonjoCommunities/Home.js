79e68fc2dcb707758d34537e1e5f537e32abc8d0
Wesley Scholl <clients@konjoweb.com>
2021-02-07 13:52:51 -0500

    Revert "updated bottom nav and header nav, need to fix extra nav screens"
    This reverts commit d0823e743c1164909aeac9cc0d99a7d7730a2cf3.

--- KonjoCommunities/Home.js
+++ KonjoCommunities/Home.js
@@ -305,7 +305,7 @@ class HomeScreen extends React.Component {
                                 <View style={styles.inputContainer}>
                                     <TextInput
                                         style={styles.textInput}
-                                        placeholder="Konjos"
+                                        placeholder="Verb, Activity"
                                         name="search"
                                         id="search"
                                         onChangeText={this.handleChange}
